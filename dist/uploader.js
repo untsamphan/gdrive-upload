37f9ee20dd5e165e292fe63394a91fe86a1cb66c
Samphan Raruenrom <untsamphan@gmail.com>
2021-10-20 18:15:13 +0700

    Use FileReader to read in chunks

--- dist/uploader.js
+++ dist/uploader.js
@@ -8,13 +8,15 @@ var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, ge
         step((generator = generator.apply(thisArg, _arguments || [])).next());
     });
 };
-const GduChunkSize = 5 * 1024 * 1024;
-function gdUpload({ file, token, folder, buf, onProgress }) {
+const DEFAULT_CHUNK_SIZE = 5 * 1024 * 1024;
+function gdUpload({ file, token, folder, chunkSize, onProgress }) {
     return __awaiter(this, void 0, void 0, function* () {
         if (!(file && token))
             throw "bad param";
         if (!onProgress)
             onProgress = (_v) => { };
+        if (!chunkSize)
+            chunkSize = DEFAULT_CHUNK_SIZE;
         const body = { mimeType: file.type, name: file.name };
         if (folder)
             body.parents = [folder];
@@ -34,49 +36,48 @@ function gdUpload({ file, token, folder, buf, onProgress }) {
         }
         if (!response.ok)
             throw "init fetch status: " + response.status;
-        const location = response.headers.get("location");
+        const location = response.headers.get("location") || "";
         if (!location)
             throw "no location";
-        doUpload(location, file, buf, onProgress);
+        uploadChunk(0);
+        function uploadChunk(start) {
+            return __awaiter(this, void 0, void 0, function* () {
+                onProgress(start / file.size);
+                let end = start + chunkSize;
+                if (end > file.size)
+                    end = file.size;
+                console.log("start:end", start, end);
+                const reader = new FileReader();
+                const blob = file.slice(start, end);
+                reader.readAsArrayBuffer(blob);
+                let response;
+                try {
+                    response = yield fetch(location, {
+                        method: "PUT",
+                        headers: {
+                            "Content-Range": `bytes ${start}-${end - 1}/${file.size}`
+                        },
+                        body: blob
+                    });
+                }
+                catch (error) {
+                    throw `chunk fetch: (${error})`;
+                }
+                if (response.status == 308) { // upload next chunk
+                    const r = response.headers.get("Range");
+                    if (!r)
+                        throw "no range in response";
+                    const realEnd = parseInt(r.substr(r.indexOf("-") + 1));
+                    if (!Number.isInteger(realEnd) || realEnd < start)
+                        throw "bad range: " + r;
+                    uploadChunk(realEnd + 1);
+                }
+                else if (response.ok)
+                    onProgress(1); // all done
+                else
+                    throw "chunk status: " + response.status;
+            });
+        }
     });
-}
-function doUpload(location, file, buf, onProgress) {
-    uploadChunk(0);
-    function uploadChunk(start) {
-        return __awaiter(this, void 0, void 0, function* () {
-            onProgress(start / file.size);
-            let end = start + GduChunkSize;
-            if (end > file.size)
-                end = file.size;
-            console.log("start:end", start, end);
-            const data = buf.slice(start, end);
-            let response;
-            try {
-                response = yield fetch(location, {
-                    method: "PUT",
-                    headers: {
-                        "Content-Range": `bytes ${start}-${end - 1}/${file.size}`
-                    },
-                    body: data
-                });
-            }
-            catch (error) {
-                throw `chunk fetch: (${error})`;
-            }
-            if (response.status == 308) {
-                const r = response.headers.get("Range");
-                if (!r)
-                    throw "no range in response";
-                const realEnd = parseInt(r.substr(r.indexOf("-") + 1));
-                if (!Number.isInteger(realEnd) || realEnd < start)
-                    throw "bad range: " + r;
-                uploadChunk(realEnd + 1);
-            }
-            else if (response.ok)
-                onProgress(1); // Done
-            else
-                throw "chunk status: " + response.status;
-        });
-    }
 }
 //# sourceMappingURL=uploader.js.map
--- src/uploader.ts
+++ src/uploader.ts
@@ -1,17 +1,19 @@
-const GduChunkSize = 5 * 1024 * 1024;
+const DEFAULT_CHUNK_SIZE = 5 * 1024 * 1024;
 type GduProgressFn = (value: number) => void;
 
 interface GduOptions {
   file: File;
-  token: string; // from Google App
-  folder: string; // folder id
-  buf: ArrayBuffer;
+  token: string; // from Google api
+  folder: string; // parent folder id
+  chunkSize: number;
   onProgress: GduProgressFn;
 }
 
-async function gdUpload({ file, token, folder, buf, onProgress }: GduOptions) {
+async function gdUpload({ file, token, folder, chunkSize, onProgress }
+  : GduOptions) {
   if (!(file && token)) throw "bad param";
-  if (!onProgress) onProgress = (_v: number) => { };
+  if (!onProgress) onProgress = (_v: number) => {};
+  if (!chunkSize) chunkSize = DEFAULT_CHUNK_SIZE;
 
   const body = { mimeType: file.type, name: file.name } as any;
   if (folder) body.parents = [folder];
@@ -32,40 +34,42 @@ async function gdUpload({ file, token, folder, buf, onProgress }: GduOptions) {
     throw "init fetch: " + error;
   }
   if (!response.ok) throw "init fetch status: " + response.status;
-  const location = response.headers.get("location");
+  const location = response.headers.get("location") || "";
   if (!location) throw "no location";
-  doUpload(location, file, buf, onProgress);
-}
 
-function doUpload(location: string, file: File, buf: ArrayBuffer, onProgress: GduProgressFn) {
   uploadChunk(0);
 
   async function uploadChunk(start: number) {
     onProgress(start / file.size);
-    let end = start + GduChunkSize;
+
+    let end = start + chunkSize;
     if (end > file.size) end = file.size;
     console.log("start:end", start, end);
-    const data = buf.slice(start, end);
-    let response: Response;
 
+    const reader = new FileReader();
+    const blob = file.slice(start, end);
+    reader.readAsArrayBuffer(blob);
+
+    let response: Response;
     try {
       response = await fetch(location, {
         method: "PUT",
         headers: {
           "Content-Range": `bytes ${start}-${end-1}/${file.size}`
         },
-        body: data
+        body: blob
       });
     } catch(error) {
       throw `chunk fetch: (${error})`;
     }
-    if (response.status == 308) {
+    if (response.status == 308) { // upload next chunk
       const r = response.headers.get("Range");
       if (!r) throw "no range in response";
-      const realEnd = parseInt(r.substr(r.indexOf("-")+1));
+      const realEnd = parseInt(r.substr(r.indexOf("-") + 1));
       if (!Number.isInteger(realEnd) || realEnd < start) throw "bad range: " + r;
       uploadChunk(realEnd + 1);
-    } else if (response.ok) onProgress(1); // Done
+
+    } else if (response.ok) onProgress(1); // all done
     else throw "chunk status: " + response.status;
   }
 }
